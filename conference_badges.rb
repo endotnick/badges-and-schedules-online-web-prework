554b857277b7fef585e99ab5ca7a3f80db3ab76c
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-20 02:23:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,12 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(attendees)
+  messages = []
+  for name in attendees do
+    messages << badge_maker(name)
+  end
+  messages
 end
 
 def assign_rooms
